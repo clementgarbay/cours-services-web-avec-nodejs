9f6309f72260aed96ba047d214daf4a2f315043f
Clément Garbay <clementgarbay@gmail.com>
2017-04-12 10:54:21 +0200

    Add TP1, TP2, TP3

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+const commander = require('commander');
+const fs = require('fs');
+
 const contactFile = process.env.npm_package_config_contacts;
+const contacts = require(contactFile);
+
+commander
+  .command('list')
+  .description('List all contacts')
+  .action(() => {
+    contacts.forEach(c => console.log(`${c.lastName.toUpperCase()} ${c.firstName}`));
+  });
+
+commander
+  .command('add <firstName> <lastName>')
+  .description('Add a new contact')
+  .action((firstName, lastName) => {
+    contacts.push({
+      firstName,
+      lastName,
+      id: 'TEST',
+    });
+
+    fs.writeFile(contactFile, JSON.stringify(contacts, null, 4), (err) => {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+  });
+
+commander.parse(process.argv);
+
+if (process.argv.length < 3) {
+  commander.help();
+}
